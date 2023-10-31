631247f8cc9cd8cc2bae4d6aa571e3dc02367308
lester <sinpo828@126.com>
2023-10-31 23:03:57 +0800

    update: add compression/metrics

--- go.mod
+++ go.mod
@@ -3,15 +3,28 @@ module github.com/cocktail828/go-tools
 go 1.19
 
 require (
+	github.com/DataDog/zstd v1.5.5
 	github.com/avast/retry-go/v4 v4.5.0
+	github.com/klauspost/compress v1.17.2
+	github.com/pierrec/lz4 v2.6.1+incompatible
 	github.com/pkg/errors v0.9.1
+	github.com/prometheus/client_golang v1.17.0
 	github.com/sirupsen/logrus v1.9.3
 	github.com/stretchr/testify v1.8.2
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/frankban/quicktest v1.14.6 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	github.com/prometheus/client_model v0.4.1-0.20230718164431-9a2bf3000d16 // indirect
+	github.com/prometheus/common v0.44.0 // indirect
+	github.com/prometheus/procfs v0.11.1 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ metrics/server_test.go
@@ -0,0 +1,49 @@
+package metrics_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/cocktail828/go-tools/metrics"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func TestPrometheus(t *testing.T) {
+	srv := metrics.NewMetricsServer()
+	go srv.Run(":8080")
+
+	// gauge
+	gauge := srv.RegisterGauge(metrics.CollectorOpt{
+		Namespace: "dbproxy",
+		Subsystem: "accesser",
+		Name:      "dts_gauge",
+	})
+	gauge.Set(1)
+
+	// counter
+	counter := srv.RegisterCounter(metrics.CollectorOpt{
+		Namespace: "dbproxy",
+		Subsystem: "accesser",
+		Name:      "dts_counter",
+	})
+	counter.Add(100)
+
+	vec := srv.RegisterCounterVec(metrics.CollectorOpt{
+		Namespace: "dbproxy",
+		Subsystem: "accesser",
+		Name:      "dts_counter_vec",
+	}, []string{"code", "desc"})
+	vec.WithLabelValues("18934", "kajsdf sdkfj").Inc()
+
+	// histogram
+	histogram := srv.RegisterHistogram(metrics.CollectorOpt{
+		Namespace: "dbproxy",
+		Subsystem: "accesser",
+		Name:      "dts_histogram",
+	}, prometheus.ExponentialBuckets(0.001, 10, 5))
+	timer := prometheus.NewTimer(histogram)
+	time.Sleep(time.Millisecond * 100)
+	timer.ObserveDuration()
+
+	time.Sleep(time.Hour)
+}
