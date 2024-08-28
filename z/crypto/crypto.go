26d580409c26fc9dd8be80943e782ab1a9359edd
sinpo828 <sinpo828@126.com>
2024-08-28 18:04:39 +0800

    update: add aes crypto

--- z/crypto/crypto.go
+++ z/crypto/crypto.go
@@ -1,10 +1,14 @@
 package crypto
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/md5" // #nosec
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/hex"
+	"io"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -14,6 +18,12 @@ func SHA256(src string, salt string) string {
 	h.Write([]byte(src + salt))
 	sum := h.Sum(nil)
 	return hex.EncodeToString(sum)
+}
+
+func MD5(str string) string {
+	h := md5.New()
+	data := h.Sum([]byte(str))
+	return hex.EncodeToString(data)
 }
 
 // PasswordEncrypt encrypt password
@@ -37,8 +47,35 @@ func Base64Encode(pwd string) string {
 	return base64.StdEncoding.EncodeToString([]byte(pwd))
 }
 
-func MD5(str string) string {
-	h := md5.New()
-	data := h.Sum([]byte(str))
-	return hex.EncodeToString(data)
+func AESEncrypt(plaintext, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	return ciphertext, nil
+}
+
+func AESDecrypt(ciphertext, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return ciphertext, nil
 }
