7eadde53444b5447015dda49efc63cf2a52b5b4f
lester <sinpo828@126.com>
2024-10-17 23:51:12 +0800

    update: refactor chain

--- z/chain/chain.go
+++ z/chain/chain.go
@@ -1,26 +1,68 @@
 package chain
 
-import "log"
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"math"
 
-type Handler func(*Context)
+	"github.com/pkg/errors"
+)
+
+const (
+	// abortIndex represents a typical value used in abort functions.
+	abortIndex int8 = math.MaxInt8 >> 1
+
+	// global key
+	globalKey = "__global__"
+)
+
+type Handler interface {
+	Name() string
+	Execute(ctx *Context)
+}
 
 type Chain struct {
-	Handlers []Handler
+	Logger   *slog.Logger
+	Prepare  func(context.Context) (any, error)
+	handlers []Handler
 }
 
-func (c *Chain) Use(h Handler) {
-	c.Handlers = append(c.Handlers, h)
+func (chain *Chain) Use(handlers ...Handler) {
+	if len(handlers) >= int(abortIndex) {
+		panic("too many handlers")
+	}
+	chain.handlers = append(chain.handlers, handlers...)
 }
 
-func (c *Chain) Handle(ctx *Context) {
+func (chain *Chain) Handle(ctx context.Context) error {
+	if len(chain.handlers) == 0 {
+		return errors.New("no handlers found")
+	}
+
+	c := &Context{
+		Context: ctx,
+		chain:   chain,
+	}
 	defer func() {
 		if err := recover(); err != nil {
-			log.Printf("oops! chain handlers(%v) panic: %v", ctx.index, err)
+			chain.Logger.Error(fmt.Sprintf("oops! chain handlers(%v) panic: %v", c.index, err))
 		}
 	}()
 
-	for ctx.index < len(c.Handlers) {
-		c.Handlers[ctx.index](ctx)
-		ctx.index++
+	if chain.Prepare != nil {
+		req, err := chain.Prepare(ctx)
+		if err != nil {
+			return err
+		}
+		c.request = req
 	}
+
+	for _, h := range chain.handlers {
+		if c.index >= int8(len(c.chain.handlers)) {
+			break
+		}
+		h.Execute(c)
+	}
+	return c.Error()
 }
--- z/chain/context.go
+++ z/chain/context.go
@@ -1,37 +1,63 @@
 package chain
 
 import (
-	"math"
+	"context"
+	"log/slog"
 	"sync"
 
 	"github.com/cocktail828/go-tools/z/errcode"
+	"github.com/cocktail828/go-tools/z/reflectx"
+	"github.com/pkg/errors"
 )
 
 type Context struct {
+	context.Context
+	chain   *Chain
+	index   int8
+	request any
+	errdesc error
 	data    sync.Map
-	index   int
-	errdesc errcode.Error
 }
 
-func (c *Context) Set(k, v any) {
-	c.data.Store(k, v)
-}
-
-func (c *Context) Get(k any) (any, bool) {
-	return c.data.Load(k)
-}
-
-// stop iterator immediately
+func (c *Context) IsAborted() bool { return !reflectx.IsNil(c.errdesc) }
 func (c *Context) Abort() {
-	c.index = math.MaxInt16
+	cur := c.chain.handlers[c.index]
+	c.errdesc = errors.Errorf("chain abort at(%v) unexpectlly", cur.Name())
+	c.index = abortIndex
 }
 
-// same like 'Abort', but set errcode
-func (c *Context) AbortWith(code errcode.Code, format string, args ...any) {
-	c.errdesc = errcode.Errorf(code, format, args...)
-	c.index = math.MaxInt16
+func (c *Context) AbortWithError(err *errcode.Error) {
+	c.index = abortIndex
+	c.errdesc = err
 }
 
-func (c *Context) Error() error {
-	return c.errdesc
+func (c *Context) Next() {
+	c.index++
+	for c.index < int8(len(c.chain.handlers)) {
+		c.chain.handlers[c.index].Execute(c)
+		c.index++
+	}
 }
+
+func (c *Context) Logger() *slog.Logger {
+	return c.chain.Logger
+}
+
+func (c *Context) SetLocal(v any) {
+	if c.index < int8(len(c.chain.handlers)) {
+		cur := c.chain.handlers[c.index]
+		c.data.Store(cur.Name(), v)
+	}
+}
+
+func (c *Context) GetLocal() (any, bool) {
+	if c.index < int8(len(c.chain.handlers)) {
+		cur := c.chain.handlers[c.index]
+		return c.data.Load(cur.Name())
+	}
+	return nil, false
+}
+
+func (c *Context) Set(v any)        { c.data.Store(globalKey, v) }
+func (c *Context) Get() (any, bool) { return c.data.Load(globalKey) }
+func (c *Context) Error() error     { return c.errdesc }
