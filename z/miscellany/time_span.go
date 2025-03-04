fa1bfb573ba61c5e4e0eb7bef44a4dbfc7e23a86
sinpo828 <sinpo828@126.com>
2025-03-04 22:52:43 +0800

    update: miscellany fix bug

--- z/miscellany/time_span.go
+++ z/miscellany/time_span.go
@@ -2,17 +2,13 @@ package miscellany
 
 import (
 	"context"
-	"sync"
 	"time"
-
-	"github.com/pkg/errors"
 )
 
 // TimeRecorder provides methods to record time duration
 type TimeRecorder struct {
 	start time.Time
 	last  time.Time
-	meta  sync.Map
 }
 
 // NewTimeRecorder creates a new TimeRecorder
@@ -21,28 +17,6 @@ func NewTimeRecorder() *TimeRecorder {
 		start: time.Now(),
 		last:  time.Now(),
 	}
-}
-
-func (tr *TimeRecorder) Mark(name string) time.Duration {
-	span := tr.Duration()
-	tr.meta.Store(name, span)
-	return span
-}
-
-func (tr *TimeRecorder) Load(name string) time.Duration {
-	if val, ok := tr.meta.Load(name); ok {
-		return val.(time.Duration)
-	}
-	return 0
-}
-
-func (tr *TimeRecorder) Records() map[string]time.Duration {
-	meta := map[string]time.Duration{}
-	tr.meta.Range(func(key, value any) bool {
-		meta[key.(string)] = value.(time.Duration)
-		return true
-	})
-	return meta
 }
 
 // Duration returns the duration from last record
@@ -64,17 +38,17 @@ func (tr *TimeRecorder) Elapse() time.Duration {
 // LongTermChecker checks we receive at least one msg in d duration. If not, checker
 // will print a warn message.
 type LongTermChecker struct {
-	d      time.Duration
-	t      *time.Ticker
-	cb     func() // the `cb` will be called on timeout
-	ctx    context.Context
-	cancel context.CancelFunc
-	start  time.Time
+	d       time.Duration
+	t       *time.Ticker
+	cb      func()
+	ctx     context.Context
+	cancel  context.CancelFunc
+	elapsed time.Duration
 }
 
 // NewLongTermChecker creates a long term checker specified name, checking interval and warning string to print
-func NewLongTermChecker(ctx context.Context, d time.Duration, cb func()) *LongTermChecker {
-	ctx, cancel := context.WithCancel(ctx)
+func NewLongTermChecker(d time.Duration, cb func()) *LongTermChecker {
+	ctx, cancel := context.WithCancel(context.Background())
 	return &LongTermChecker{
 		d:      d,
 		cb:     cb,
@@ -85,10 +59,11 @@ func NewLongTermChecker(ctx context.Context, d time.Duration, cb func()) *LongTe
 
 // Start starts the check process
 func (c *LongTermChecker) Start() {
-	c.t = time.NewTicker(c.d)
-	c.start = time.Now()
 	go func() {
+		c.t = time.NewTicker(c.d)
 		defer c.t.Stop()
+		startAt := time.Now()
+		defer func() { c.elapsed = time.Since(startAt) }()
 		for {
 			select {
 			case <-c.ctx.Done():
@@ -100,21 +75,22 @@ func (c *LongTermChecker) Start() {
 	}()
 }
 
-// Check resets the time ticker
-func (c *LongTermChecker) Check() error {
-	if c.t == nil {
-		return errors.New("forget call `Start()` ???")
-	}
-	c.t.Reset(c.d)
-	return nil
-}
-
 // Stop stops the checker
 func (c *LongTermChecker) Stop() {
 	c.cancel()
+	if c.t != nil {
+		c.t.Stop()
+	}
+}
+
+// Check resets the time ticker
+func (c *LongTermChecker) Check() {
+	if c.t != nil {
+		c.t.Reset(c.d)
+	}
 }
 
 // Elapse report the time duration of the checker
 func (c *LongTermChecker) Elapse() time.Duration {
-	return time.Since(c.start)
+	return c.elapsed
 }
