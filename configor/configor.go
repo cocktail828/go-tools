4210aa8964dc749a108da5755ab96446f6e1c882
xbli10 <xbli10@iflytek.com>
2025-09-05 17:59:33 +0800

    update: add LoadWithUnmarshaller funcs for configor

--- configor/configor.go
+++ configor/configor.go
@@ -1,7 +1,9 @@
 package configor
 
 import (
+	"os"
 	"reflect"
+	"strconv"
 
 	"github.com/BurntSushi/toml"
 	"github.com/go-playground/validator/v10"
@@ -9,58 +11,155 @@ import (
 )
 
 type Configor struct {
-	LoadEnv   bool            // 是否读取环境变量
-	EnvPrefix string          // 环境变量前缀
-	Unmarshal Unmarshal       // 解析器
-	Validator func(any) error // 校验器
+	LoadEnv      bool                    // 是否读取环境变量
+	EnvPrefix    string                  // 环境变量前缀
+	Unmarshaller func([]byte, any) error // 解析器
+	Validator    func(any) error         // 校验器
 }
 
-var cfgor = &Configor{
-	LoadEnv:   false,
-	EnvPrefix: "",
-	Unmarshal: toml.Unmarshal,
-	Validator: validator.New().Struct,
+type Pair struct {
+	data         []byte
+	unmarshaller func([]byte, any) error
 }
 
-func (c *Configor) Load(dst any, data ...[]byte) error {
-	pairs := make([]pair, 0, len(data))
-	for _, d := range data {
-		pairs = append(pairs, pair{data: d, unmarshal: c.Unmarshal})
-	}
-	if err := c.internalLoad(dst, pairs...); err != nil {
-		return err
-	}
-	if c.Validator != nil {
-		return c.Validator(dst)
-	}
-	return nil
-}
-
-func Load(dst any, data ...[]byte) error {
-	return cfgor.Load(dst, data...)
-}
-
-type Unmarshal func([]byte, any) error
-
-type pair struct {
-	data      []byte
-	unmarshal Unmarshal
-}
-
-func (c *Configor) internalLoad(dst any, pairs ...pair) error {
-	defaultValue := reflect.Indirect(reflect.ValueOf(dst))
-	if !defaultValue.CanAddr() {
-		return errors.Errorf("config %v must be addressable", dst)
+func (c *Configor) bindEnv(in any) error {
+	v := reflect.ValueOf(in)
+	if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Struct {
+		return errors.Errorf("input must be a pointer to a struct")
 	}
 
-	if err := BindEnv(dst, WithPrefix(c.EnvPrefix), WithSkipEnv(!c.LoadEnv)); err != nil {
-		return errors.Wrap(err, "fail to process env or defaults")
-	}
+	v = v.Elem()
+	t := v.Type()
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		structField := t.Field(i)
 
-	for _, val := range pairs {
-		if err := val.unmarshal(val.data, dst); err != nil {
-			return errors.Wrap(err, "fail to unmarshal data")
+		if field.Kind() == reflect.Ptr {
+			if field.IsNil() {
+				field.Set(reflect.New(field.Type().Elem()))
+			}
+			if field.Elem().Kind() == reflect.Ptr {
+				return errors.Errorf("unsupported nested pointer type: %s", field.Type())
+			}
+			field = field.Elem()
+		}
+
+		if field.Kind() == reflect.Struct {
+			if err := c.bindEnv(field.Addr().Interface()); err != nil {
+				return err
+			}
+			continue
+		}
+
+		envName := structField.Tag.Get("env")
+		if envName != "" && envName != "-" && c.EnvPrefix != "" {
+			envName = c.EnvPrefix + "_" + envName
+		}
+
+		envVal := ""
+		if c.LoadEnv && envName != "" && envName != "-" {
+			envVal = os.Getenv(envName)
+		}
+
+		if envVal == "" {
+			envVal = structField.Tag.Get("default")
+		}
+
+		if envVal == "" {
+			continue
+		}
+
+		switch field.Kind() {
+		case reflect.String:
+			field.SetString(envVal)
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			intValue, err := strconv.ParseInt(envVal, 10, 64)
+			if err != nil {
+				return errors.Errorf("error parsing %s as int64: %v", envName, err)
+			}
+			field.SetInt(intValue)
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			intValue, err := strconv.ParseUint(envVal, 10, 64)
+			if err != nil {
+				return errors.Errorf("error parsing %s as uint64: %v", envName, err)
+			}
+			field.SetUint(intValue)
+		case reflect.Float32:
+			floatValue, err := strconv.ParseFloat(envVal, 32)
+			if err != nil {
+				return errors.Errorf("error parsing %s as float32: %v", envName, err)
+			}
+			field.SetFloat(floatValue)
+		case reflect.Float64:
+			floatValue, err := strconv.ParseFloat(envVal, 64)
+			if err != nil {
+				return errors.Errorf("error parsing %s as float64: %v", envName, err)
+			}
+			field.SetFloat(floatValue)
+		case reflect.Bool:
+			boolValue, err := strconv.ParseBool(envVal)
+			if err != nil {
+				return errors.Errorf("error parsing %s as bool: %v", envName, err)
+			}
+			field.SetBool(boolValue)
+		default:
+			// return errors.Errorf("unsupported field type: %s", field.Kind())
 		}
 	}
 	return nil
+}
+
+func (c *Configor) Load(v any, data ...[]byte) error {
+	pairs := make([]Pair, 0, len(data))
+	for _, d := range data {
+		pairs = append(pairs, Pair{d, c.Unmarshaller})
+	}
+	return c.LoadWithUnmarshaller(v, pairs...)
+}
+
+func (c *Configor) LoadWithUnmarshaller(v any, pairs ...Pair) error {
+	for i, d := range pairs {
+		if d.unmarshaller == nil {
+			return errors.Errorf("unmarshaller is nil at index %d", i)
+		}
+	}
+
+	if riv := reflect.Indirect(reflect.ValueOf(v)); !riv.CanAddr() {
+		return errors.Errorf("target %v must be addressable", v)
+	}
+
+	if err := c.bindEnv(v); err != nil {
+		return err
+	}
+
+	for _, p := range pairs {
+		if err := p.unmarshaller(p.data, v); err != nil {
+			return err
+		}
+	}
+
+	if c.Validator != nil {
+		return c.Validator(v)
+	}
+	return nil
+}
+
+func Load(v any, data ...[]byte) error {
+	cfgor := &Configor{
+		LoadEnv:      false,
+		EnvPrefix:    "",
+		Unmarshaller: toml.Unmarshal,
+		Validator:    validator.New().Struct,
+	}
+	return cfgor.Load(v, data...)
+}
+
+func LoadWithUnmarshaller(v any, pairs ...Pair) error {
+	cfgor := &Configor{
+		LoadEnv:      false,
+		EnvPrefix:    "",
+		Unmarshaller: toml.Unmarshal,
+		Validator:    validator.New().Struct,
+	}
+	return cfgor.LoadWithUnmarshaller(v, pairs...)
 }
--- configor/configor_test.go
+++ configor/configor_test.go
@@ -1,9 +1,11 @@
 package configor
 
 import (
+	"log"
 	"os"
 	"testing"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -113,4 +115,93 @@ func TestLoadConfigurationWithLoadEnvFalse(t *testing.T) {
 	assert.Equal(t, cfg.DB.User, "root", "DB.User should remain default when LoadEnv=false")
 	assert.EqualValues(t, cfg.DB.Port, 3306, "DB.Port should remain default when LoadEnv=false")
 	assert.Equal(t, cfg.DB.Password, "", "DB.Password should remain default when LoadEnv=false")
+}
+
+type Service struct {
+	Version string `env:"POLARIS_VERSION" validate:"required"`
+	AppHost string `env:"APP_HOST" validate:"required"`
+	AppPort string `env:"APP_PORT" validate:"required"`
+}
+
+type xConfig struct {
+	Companion string  `env:"POLARIS_COMPANION" default:"companion_value" validate:"required"`
+	Project   int64   `env:"POLARIS_PROJECT" validate:"required"`
+	Group     float32 `env:"POLARIS_GROUP" validate:"required"`
+	Service   Service
+}
+
+func TestBind(t *testing.T) {
+	m := MMP{
+		"POLARIS_PROJECT": "12345",
+		"POLARIS_GROUP":   "3.14",
+		"POLARIS_VERSION": "version_value",
+		"APP_HOST":        "host_value",
+		"APP_PORT":        "port_value",
+	}
+	m.SetEnv()
+	defer m.ResetEnv()
+
+	c := Configor{
+		LoadEnv:   true,
+		EnvPrefix: "",
+	}
+
+	var cfg xConfig
+	if err := c.bindEnv(&cfg); err != nil {
+		log.Fatalf("Error binding env: %v\n", err)
+	}
+
+	assert.EqualValues(t, xConfig{
+		Companion: "companion_value",
+		Project:   12345,
+		Group:     3.14,
+		Service: Service{
+			Version: "version_value",
+			AppHost: "host_value",
+			AppPort: "port_value",
+		},
+	}, cfg)
+	assert.NoError(t, validator.New().Struct(cfg))
+}
+
+type TestStructWithPointer struct {
+	Name *string `env:"NAME" default:"default_name"`
+	Age  *int    `env:"AGE" default:"20"`
+	Flag *bool   `env:"FLAG" default:"true"`
+}
+
+func TestBindEnvWithPointerFields(t *testing.T) {
+	var nilPtrTest TestStructWithPointer
+	assert.Nil(t, nilPtrTest.Name)
+	assert.Nil(t, nilPtrTest.Age)
+	assert.Nil(t, nilPtrTest.Flag)
+
+	c := Configor{}
+	if err := c.bindEnv(&nilPtrTest); err != nil {
+		assert.FailNow(t, "failed to bind nil pointer fields", err.Error())
+	}
+	assert.Equal(t, "default_name", *nilPtrTest.Name)
+	assert.Equal(t, 20, *nilPtrTest.Age)
+	assert.True(t, *nilPtrTest.Flag)
+
+	envPtrTest := TestStructWithPointer{
+		Name: new(string),
+		Age:  new(int),
+		Flag: new(bool),
+	}
+	m := MMP{
+		"NAME": "env_name",
+		"AGE":  "30",
+		"FLAG": "true",
+	}
+	m.SetEnv()
+	defer m.ResetEnv()
+
+	c.LoadEnv = true
+	if err := c.bindEnv(&envPtrTest); err != nil {
+		assert.FailNow(t, "failed to bind env to pointer fields", err.Error())
+	}
+	assert.Equal(t, "env_name", *envPtrTest.Name)
+	assert.Equal(t, 30, *envPtrTest.Age)
+	assert.True(t, *envPtrTest.Flag)
 }
