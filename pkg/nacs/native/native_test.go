a9198611655280b005c38d4c053fcbcabe32c852
lester <lester@nengneng.20>
2025-10-03 16:37:22 +0800

    update: refactor native.tests

--- pkg/nacs/native/native_test.go
+++ pkg/nacs/native/native_test.go
@@ -11,64 +11,49 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNativeRelative(t *testing.T) {
-	tempFilePath := "test_config.txt"
-	defer os.Remove(tempFilePath)
-
-	data := []byte("hello world")
-	z.Must(os.WriteFile(tempFilePath, data, os.ModePerm))
-
-	// create configor
-	u, err := url.ParseRequestURI("native://localhost/" + tempFilePath + "?relative=true")
-	z.Must(err)
-
-	configor, err := NewNativeConfigor(u)
-	z.Must(err)
-	defer configor.Close()
-
-	value, err := configor.Load()
-	z.Must(err)
-	assert.Equal(t, string(data), string(value))
-}
-
 func TestNative(t *testing.T) {
-	tempFilePath := "/tmp/test_config.txt"
-	defer os.Remove(tempFilePath)
+	tests := map[string]string{
+		"/tmp/test_config.txt": "native://localhost/tmp/test_config.txt",
+		"./test_config.txt":    "native://localhost/test_config.txt?relative=true",
+	}
 
-	data := []byte("hello world")
-	z.Must(os.WriteFile(tempFilePath, data, os.ModePerm))
+	for filePath, uri := range tests {
+		tempFilePath := filePath
+		defer os.Remove(tempFilePath)
 
-	// create configor
-	u, err := url.ParseRequestURI("native://localhost" + tempFilePath)
-	z.Must(err)
+		data := []byte("hello world")
+		z.Must(os.WriteFile(tempFilePath, data, os.ModePerm))
 
-	configor, err := NewNativeConfigor(u)
-	z.Must(err)
-	defer configor.Close()
+		// create configor
+		u, err := url.ParseRequestURI(uri)
+		z.Must(err)
 
-	value, err := configor.Load()
-	z.Must(err)
-	assert.Equal(t, string(data), string(value))
-
-	// monitor
-	data = []byte("updated_value")
-	ctx, cancel := context.WithCancel(context.Background())
-	_, err = configor.Monitor(func(name string, payload []byte, err error) {
-		assert.NoError(t, err, "Monitor callback should not return error")
+		configor, err := NewNativeConfigor(u)
+		z.Must(err)
+		defer configor.Close()
 
 		value, err := configor.Load()
 		z.Must(err)
 		assert.Equal(t, string(data), string(value))
-		cancel()
-	})
-	z.Must(err)
 
-	time.Sleep(time.Millisecond * 500)
-	z.Must(os.WriteFile(tempFilePath, data, os.ModePerm))
+		// monitor
+		data = []byte("updated_value")
+		ctx, cancel := context.WithCancel(context.Background())
+		_, err = configor.Monitor(func(name string, payload []byte, err error) {
+			assert.NoError(t, err, "Monitor callback should not return error")
 
-	select {
-	case <-ctx.Done():
-	case <-time.After(time.Second * 2):
-		t.Fatal("Monitor did not trigger within timeout period")
+			assert.Equal(t, string(data), string(payload))
+			cancel()
+		})
+		z.Must(err)
+
+		time.Sleep(time.Millisecond * 500)
+		z.Must(os.WriteFile(tempFilePath, data, os.ModePerm))
+
+		select {
+		case <-ctx.Done():
+		case <-time.After(time.Second * 2):
+			t.Fatal("Monitor did not trigger within timeout period")
+		}
 	}
 }
