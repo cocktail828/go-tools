585db89534d146b98fbd2b15938b266579a7648f
xbli10 <xbli10@iflytek.com>
2026-01-12 14:50:35 +0800

    [xlog] add missed hcl/toml tag for Logger

--- xlog/lumberjack.go
+++ xlog/lumberjack.go
@@ -84,27 +84,27 @@ type Logger struct {
 	// Filename is the file to write logs to.  Backup log files will be retained
 	// in the same directory.  It uses <processname>-lumberjack.log in
 	// os.TempDir() if empty.
-	Filename string `json:"filename" yaml:"filename"`
+	Filename string `hcl:"filename" json:"filename" toml:"filename" yaml:"filename"`
 
 	// MaxSize is the maximum size in megabytes of the log file before it gets
 	// rotated. It defaults to 100 megabytes.
-	MaxSize int `json:"maxsize" yaml:"maxsize"`
+	MaxSize int `hcl:"maxsize" json:"maxsize" toml:"maxsize" yaml:"maxsize"`
 
 	// MaxAge is the maximum number of days to retain old log files based on the
 	// timestamp encoded in their filename.  Note that a day is defined as 24
 	// hours and may not exactly correspond to calendar days due to daylight
 	// savings, leap seconds, etc. The default is not to remove old log files
 	// based on age.
-	MaxAge int `json:"maxage" yaml:"maxage"`
+	MaxAge int `hcl:"maxage" json:"maxage" toml:"maxage" yaml:"maxage"`
 
 	// MaxBackups is the maximum number of old log files to retain.  The default
 	// is to retain all old log files (though MaxAge may still cause them to get
 	// deleted.)
-	MaxBackups int `json:"maxbackups" yaml:"maxbackups"`
+	MaxBackups int `hcl:"maxbackups" json:"maxbackups" toml:"maxbackups" yaml:"maxbackups"`
 
 	// Compress determines if the rotated log files should be compressed
 	// using gzip. The default is not to perform compression.
-	Compress bool `json:"compress" yaml:"compress"`
+	Compress bool `hcl:"compress" json:"compress" toml:"compress" yaml:"compress"`
 
 	size      int64
 	file      *os.File
